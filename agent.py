9ace7dd47f30e435c16e2f7605bdb12d7b4ed9a1
mateoSufer <mateosuarezferreira@gmail.com>
2026-06-29 12:33:22 +0200

    day 4 - policy network architecture

---
+++ agent.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+import torch
+
+class PolicyNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(4, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 2)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+
+        return x
