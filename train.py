47ee49be52dbfd89b3ad734fb93b412aaf5efe62
mateoSufer <mateosuarezferreira@gmail.com>
2026-07-01 19:10:33 +0200

    day 6 - agent successfully learns to balance the pole

---
+++ train.py
@@ -0,0 +1,50 @@
+from agent import PolicyNetwork
+import gymnasium
+import torch
+
+env = gymnasium.make("CartPole-v1")
+
+policy = PolicyNetwork()
+
+optimizer = torch.optim.Adam(policy.parameters(), lr=0.001)
+
+
+for episode in range(500):
+    observation, info = env.reset()
+    log_probs = []
+    rewards = []
+    for t in range(500):
+        state = torch.FloatTensor(observation)
+        logits = policy(state)
+        probs = torch.softmax(logits, dim= -1)
+        action = torch.multinomial(probs, 1).item()
+
+        observation, reward, terminated, truncated, info = env.step(action)
+        
+        log_prob = torch.log(probs[action])
+        log_probs.append(log_prob)
+        rewards.append(reward)
+
+        if terminated or truncated:
+            break
+    
+    returns = []
+    G = 0
+    gamma = 0.99
+    for reward in reversed(rewards):
+        G = reward + gamma * G
+        returns.insert(0, G)
+
+    returns = torch.tensor(returns)
+    returns = (returns - returns.mean()) / (returns.std() + 1e-8)
+    loss = 0
+    for log_prob, G in zip(log_probs, returns):
+        loss -= log_prob * G
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    print(f"Episode {episode}, Total Reward: {sum(rewards)}")
+
+   
